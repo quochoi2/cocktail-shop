bf2eb386f9372efd4e26236ed55ccc1d84c18ae3
quochoi2 <email@moi.com>
2024-07-31 11:27:52 +0700

    set data model and lirabry for back-end

---
+++ back/DataModel/AccountModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class AccountModel
+    {
+		public int? tId { get; set; }
+		public string? tTaiKhoan { get; set; }
+		public string? tMatKhau { get; set; }
+        public string? kTen { get; set; }
+        public string? tLoai { get; set; }
+		public string? token { get; set; }
+	}
+
+}
---
+++ back/DataModel/BillModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+	public class BillModel
+	{
+		public int? hdbId { get; set; }
+		public int? kId { get; set; }
+		public string? hdbNgayLap { get; set; }
+        public string? hdbMoTa { get; set; }
+        public List<BillDetail> list_json_chitiethoadon { get; set; }
+	}
+	public class BillDetail
+	{
+		public int? ctbId { get; set; }
+		public int? hdbId { get; set; }
+		public int? spId { get; set; }
+		public int? ctbSoLuong { get; set; }
+		public int? ctbTongTien { get; set; }
+	}
+}
